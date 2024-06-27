cd8ac389559c60496e727ad5995b726aba848908
caibucai <caibucai22@gmail.com>
2024-06-27 20:44:13 +0800

    supervoxel_clustering

--- basic/supervoxel_clustering2.cpp
+++ basic/supervoxel_clustering2.cpp
@@ -4,10 +4,10 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/segmentation/supervoxel_clustering.h>
+#include <map>
+#include <memory>
 
-// VTK include needed for drawing graph lines
-#include <vtkPolyLine.h>
-
+using namespace std;
 // Types
 typedef pcl::PointXYZRGBA PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
@@ -19,7 +19,7 @@ typedef pcl::PointCloud<PointLT> PointLCloudT;
 void addSupervoxelConnectionsToViewer(PointT &supervoxel_center,
 																			PointCloudT &adjacent_supervoxel_centers,
 																			std::string supervoxel_name,
-																			pcl::visualization::PCLVisualizer::Ptr &viewer);
+																			boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer);
 
 int main(int argc, char **argv)
 {
@@ -34,14 +34,14 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	PointCloudT::Ptr cloud(new PointCloudT);
-	// 解析参数
+	PointCloudT::Ptr cloud = std::shared_ptr<PointCloudT>();
 	pcl::console::print_highlight("Loading point cloud...\n");
 	if (pcl::io::loadPCDFile<PointT>(argv[1], *cloud))
 	{
 		pcl::console::print_error("Error loading cloud file!\n");
 		return (1);
 	}
+	cout << "point size of input: " << cloud->size() << endl;
 
 	bool disable_transform = pcl::console::find_switch(argc, argv, "--NT");
 
@@ -79,62 +79,75 @@ int main(int argc, char **argv)
 	super.setSpatialImportance(spatial_importance);
 	super.setNormalImportance(normal_importance);
 
-	// 存储超体素
-	std::map<std::uint32_t, pcl::Supervoxel<PointT>::Ptr> supervoxel_clusters;
-
+	std::map<uint32_t, pcl::Supervoxel<PointT>::Ptr> supervoxel_clusters;
+	// 该单映射容器以标签为键值存储所有超体素
 	pcl::console::print_highlight("Extracting supervoxels!\n");
 	super.extract(supervoxel_clusters);
 	pcl::console::print_info("Found %d supervoxels\n", supervoxel_clusters.size());
 
-	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-	viewer->setBackgroundColor(0, 0, 0);
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("supervoxel_clustering"));
+	viewer->setBackgroundColor(1, 1, 1);
 
 	PointCloudT::Ptr voxel_centroid_cloud = super.getVoxelCentroidCloud();
-	viewer->addPointCloud(voxel_centroid_cloud, "voxel centroids");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2.0, "voxel centroids");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 0.95, "voxel centroids");
+	cout << "voxel centroids: " << voxel_centroid_cloud->size() << endl;
+	if (0)
+	{ // 对于体素中心的可视化和保存，基本就是对原始数据的空间均匀下采样
+		viewer->addPointCloud<PointT>(voxel_centroid_cloud, "voxel centroids");
+		pcl::io::savePCDFile("voxel_centroids.pcd", *voxel_centroid_cloud);
+		viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, "voxel centroids");
+		viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 0.5, "voxel centroids");
+	}
 
 	PointLCloudT::Ptr labeled_voxel_cloud = super.getLabeledVoxelCloud();
-	viewer->addPointCloud(labeled_voxel_cloud, "labeled voxels");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 0.8, "labeled voxels");
+	if (1)
+	{ // 超体素分割结果显示与保存
+		pcl::io::savePCDFile("labeled_voxels.pcd", *labeled_voxel_cloud);
+		viewer->addPointCloud(labeled_voxel_cloud, "labeled voxels");
+		cout << "labeled voxels: " << labeled_voxel_cloud->size() << endl;
+		viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "labeled voxels");
+		// viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_OPACITY,0.8, "labeled voxels");
+	}
 
 	PointNCloudT::Ptr sv_normal_cloud = super.makeSupervoxelNormalCloud(supervoxel_clusters);
-	// We have this disabled so graph is easy to see, uncomment to see supervoxel normals
-	// viewer->addPointCloudNormals<PointNormal> (sv_normal_cloud,1,0.05f, "supervoxel_normals");
+	//
+	if (0) // 超体素对应的法线特征可视化
+		viewer->addPointCloudNormals<pcl::PointNormal>(sv_normal_cloud, 1, 0.05f, "supervoxel_normals");
 
 	pcl::console::print_highlight("Getting supervoxel adjacency\n");
-	// 构造邻接图
-	std::multimap<std::uint32_t, std::uint32_t> supervoxel_adjacency;
+	std::multimap<uint32_t, uint32_t> supervoxel_adjacency;
 	super.getSupervoxelAdjacency(supervoxel_adjacency);
-	// To make a graph of the supervoxel adjacency, we need to iterate through the supervoxel adjacency multimap
-	for (auto label_itr = supervoxel_adjacency.cbegin(); label_itr != supervoxel_adjacency.cend();)
+	cout << "size of supervoxel_adjacency: " << supervoxel_adjacency.size() << endl;
+
+	// 遍历多重映射容器构造邻接图
+	std::multimap<uint32_t, uint32_t>::iterator label_itr = supervoxel_adjacency.begin();
+	for (; label_itr != supervoxel_adjacency.end();)
 	{
-		// 获取标签
+		// 获取标签值
 		std::uint32_t supervoxel_label = label_itr->first;
-		// 根据标签索引得到超体素
+		// 根据标签索引到该超体素
 		pcl::Supervoxel<PointT>::Ptr supervoxel = supervoxel_clusters.at(supervoxel_label);
 
-		
 		// 遍历该超体素相邻超体素并以其相邻超体素中心为点集构造点云，用于后续可视化，这里的相邻超体素在多重映射容器中具有相同的键值
 		PointCloudT adjacent_supervoxel_centers;
-		for (auto adjacent_itr = supervoxel_adjacency.equal_range(supervoxel_label).first; adjacent_itr != supervoxel_adjacency.equal_range(supervoxel_label).second; ++adjacent_itr)
+		std::multimap<uint32_t, uint32_t>::iterator adjacent_itr = supervoxel_adjacency.equal_range(supervoxel_label).first;
+		for (; adjacent_itr != supervoxel_adjacency.equal_range(supervoxel_label).second; ++adjacent_itr)
 		{
 			pcl::Supervoxel<PointT>::Ptr neighbor_supervoxel = supervoxel_clusters.at(adjacent_itr->second);
 			adjacent_supervoxel_centers.push_back(neighbor_supervoxel->centroid_);
 		}
-		// Now we make a name for this polygon
+		//
 		std::stringstream ss;
 		ss << "supervoxel_" << supervoxel_label;
-		// This function is shown below, but is beyond the scope of this tutorial - basically it just generates a "star" polygon mesh from the points given
-		// 绘制超体素与其相邻超体素
+		// cout<<ss.str()<<endl;
+		// 绘制该超体素与其相邻超体素子图
 		addSupervoxelConnectionsToViewer(supervoxel->centroid_, adjacent_supervoxel_centers, ss.str(), viewer);
-		// Move iterator forward to next label
+		// 使迭代器指向下一个标签。
 		label_itr = supervoxel_adjacency.upper_bound(supervoxel_label);
 	}
 
 	while (!viewer->wasStopped())
 	{
-		viewer->spinOnce(100);
+		viewer->spinOnce();
 	}
 	return (0);
 }
@@ -142,27 +155,24 @@ int main(int argc, char **argv)
 void addSupervoxelConnectionsToViewer(PointT &supervoxel_center,
 																			PointCloudT &adjacent_supervoxel_centers,
 																			std::string supervoxel_name,
-																			pcl::visualization::PCLVisualizer::Ptr &viewer)
+																			boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer)
 {
-	vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
-	vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
-	vtkSmartPointer<vtkPolyLine> polyLine = vtkSmartPointer<vtkPolyLine>::New();
 
+	int i = 0;
 	// Iterate through all adjacent points, and add a center point to adjacent point pair
-	for (auto adjacent_itr = adjacent_supervoxel_centers.begin(); adjacent_itr != adjacent_supervoxel_centers.end(); ++adjacent_itr)
+	PointCloudT::iterator adjacent_itr = adjacent_supervoxel_centers.begin();
+	for (; adjacent_itr != adjacent_supervoxel_centers.end(); ++adjacent_itr)
 	{
-		points->InsertNextPoint(supervoxel_center.data);
-		points->InsertNextPoint(adjacent_itr->data);
+		std::stringstream ss;
+		ss << supervoxel_name << i;
+		viewer->addLine(supervoxel_center, *adjacent_itr, ss.str());
+
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 3, ss.str());
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0, 255, 0, ss.str());
+		ss << supervoxel_name << i;
+		viewer->addSphere(supervoxel_center, 0.008, 0, 0, 255, ss.str());
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_SHADING, pcl::visualization::PCL_VISUALIZER_SHADING_GOURAUD, ss.str());
+		// viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY,0.9,ss.str());
+		i++;
 	}
-	// Create a polydata to store everything in
-	vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
-	// Add the points to the dataset
-	polyData->SetPoints(points);
-	polyLine->GetPointIds()->SetNumberOfIds(points->GetNumberOfPoints());
-	for (unsigned int i = 0; i < points->GetNumberOfPoints(); i++)
-		polyLine->GetPointIds()->SetId(i, i);
-	cells->InsertNextCell(polyLine);
-	// Add the lines to the dataset
-	polyData->SetLines(cells);
-	viewer->addModelFromPolyData(polyData, supervoxel_name);
 }
