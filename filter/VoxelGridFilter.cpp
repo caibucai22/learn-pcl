760ce6d93316aa636241a3c8420dbceeb570eaf8
caibucai <caibucai22@gmail.com>
2024-07-01 16:07:07 +0800

    VoxelGridFilter

--- filter/VoxelGridFilter.cpp
+++ filter/VoxelGridFilter.cpp
@@ -1,35 +1,41 @@
-#include<iostream>
-#include<pcl/io/pcd_io.h>
-#include<pcl/point_types.h>
-#include<pcl/filters/voxel_grid.h>
-#include<pcl/visualization/pcl_visualizer.h>
-#include<boost/thread/thread.hpp>
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <boost/thread/thread.hpp>
 #include <chrono>
 #include <thread>
 
 using namespace std;
 
-int
-main3(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud < pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PCDReader reader;
-	reader.read("ro.pcd", *cloud);
-	cout << "the num of points: " << cloud->points.size() << endl;
+	if (argc < 2 || reader.read(argv[1], *cloud) < 0)
+	{
+		PCL_ERROR("load pcd failed");
+		return -1;
+	}
+	cout << "load success. the num of points: " << cloud->points.size() << endl;
 
+	// voxel grid filter
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::VoxelGrid<pcl::PointXYZ> vg;
 	vg.setInputCloud(cloud);
-	vg.setLeafSize(0.01f, 0.01f, 0.01f); // 设置最小体素边长
-	//vg.setMinimumPointsNumberPerVoxel(10); // 设置每一个体素内需要包含的最小点个数 体素内点多于指定数 使用setLeafSize() 进行计算 少于的话就重新计算最小体素边长
+	vg.setLeafSize(0.01f, 0.01f, 0.01f);
+	// vg.setMinimumPointsNumberPerVoxel(10);
 	vg.filter(*cloud_filtered);
+	// voxel grid filter
 
-	cout << "after filter the num of point: " << cloud_filtered->points.size() << endl;
-	// 保存结果
+	cout << "after filtering, the num of point: " << cloud_filtered->points.size() << endl;
+
+	// write
+
 	pcl::PCDWriter writer;
-	writer.write<pcl::PointXYZ>("2f.pcd", *cloud_filtered, false);
+	writer.write<pcl::PointXYZ>("voxelgrid_output.pcd", *cloud_filtered, false);
 
-	// 可视化
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> view(new pcl::visualization::PCLVisualizer("show cloud"));
 	int v1(0);
 	view->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
@@ -40,15 +46,15 @@ main3(int argc, char** argv)
 	view->setBackgroundColor(0.1, 0.1, 0.1, v2);
 	view->addText("filtered point cloud", 10, 10, "v2_text", v2);
 
-
 	view->addPointCloud<pcl::PointXYZ>(cloud, "sample cloud", v1);
 	view->addPointCloud<pcl::PointXYZ>(cloud_filtered, "cloud_filtered", v2);
 	view->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "sample cloud", v1);
 	view->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0, 1, 0, "sample cloud", v2);
 
-	while (!view->wasStopped()) {
+	while (!view->wasStopped())
+	{
 		view->spinOnce(100);
-		//boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+		// boost::this_thread::sleep(boost::posix_time::microseconds(100000));
 		std::this_thread::sleep_for(std::chrono::microseconds(100000));
 	}
 
